f14e7d3cbabaa883a15c4124f029628d93b7de71
pujan <pujxnp@gmail.com>
2026-03-23 14:06:27 +0530

    feat: initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,13 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), svgr()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
