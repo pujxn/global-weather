97d1424521ab6c7f37a1a7b93238ac2814658bd9
pujan <pujxnp@gmail.com>
2026-03-25 13:47:04 +0530

    Added dark mode map

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,8 +1,13 @@
 import { MapContainer, Marker, TileLayer, useMap } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import type { Coords } from "../types";
+import { useEffect } from "react";
+
+import { MaptilerLayer } from "@maptiler/leaflet-maptilersdk";
 
 const API_KEY = import.meta.env.VITE_API_KEY;
+
+const MAPTILER_API_KEY = import.meta.env.VITE_MAPTILER_API_KEY;
 
 type Props = {
   coords: Coords;
@@ -21,11 +26,9 @@ const Map = ({ coords, onMapClick, mapType }: Props) => {
       scrollWheelZoom={false}
     >
       <MapClick onMapClick={onMapClick} coords={coords} />
+      <MapTileLayer />
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <TileLayer
+        opacity={0.7}
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url={`https://tile.openweathermap.org/map/${mapType}/{z}/{x}/{y}.png?appid=${API_KEY}`}
       />
@@ -50,6 +53,24 @@ const MapClick = ({
     const { lat, lng } = e.latlng;
     onMapClick(lat, lng);
   });
+
+  return null;
+};
+
+const MapTileLayer = () => {
+  const map = useMap();
+
+  useEffect(() => {
+    const tileLayer = new MaptilerLayer({
+      style: "basic-dark",
+      apiKey: MAPTILER_API_KEY,
+    });
+    tileLayer.addTo(map);
+
+    return () => {
+      map.removeLayer(tileLayer);
+    };
+  }, [map]);
 
   return null;
 };
